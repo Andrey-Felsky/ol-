2f7e0a10f3c5fe6101e25cc17a6331d589ca58f2
Andrey Felsky <andreyfelsky@edu.univali.br>
2024-09-19 21:59:17 -0300

    Add files via upload

---
+++ github.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#include <locale.h>
+int main () {
+    setlocale (LC_ALL,"Portuguese");
+    int cont = 1;
+    while (cont <= 30){
+        cout<<"não vou colar na prova"<<endl <<endl;
+        cont++;
+}
+    return 0;
+    }
